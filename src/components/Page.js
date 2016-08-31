b8f1bab89263072d6a7d78517374387f36264122
Raphael Kottakal <raphaelsemail@gmail.com>
2016-08-31 17:39:40 +0530

    the setup

---
+++ src/components/Page.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import 'normalize.css/normalize.css';
+import '../css/main.css';
+
+import Ga from '../functions/Ga';
+
+export default class Page extends React.Component {
+
+	render() {
+
+		return(
+			<div>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut nunc tellus, mattis id elementum ultrices, consectetur ut mauris. Aenean auctor magna lectus, in porttitor est rhoncus nec. Interdum et malesuada fames ac ante ipsum primis in faucibus. Mauris magna eros, interdum in bibendum nec, vestibulum non urna. Vestibulum mattis ex dui, congue vestibulum leo mattis nec. Pellentesque eu elit at ante placerat dignissim in ut orci. Curabitur libero quam, placerat nec accumsan et, elementum id ipsum. Morbi ac tellus in est commodo lacinia. In finibus at sem vitae interdum. Nulla lobortis, turpis in egestas accumsan, tellus dui ultricies augue, vitae elementum est massa vel nisi. Maecenas quam turpis, tincidunt vitae libero et, ultrices dapibus diam. Aenean cursus, felis id tempor aliquam, diam arcu maximus i, non vestibulum augue elit eget diam. Aliquam pharetra felis non tristique ultrices.</p>
+
+				<p>Etiam ac sem eu nibh condimentum rutrum. Integer euismod, leo sit amet imperdiet ultricies, ante magna ullamcorper mauris, sed fringilla metus enim accumsan risus. Nam egestas, nunc ac venenatis placerat, magna est rutrum lorem, sit amet lobortis eros justo sed arcu. Quisque nec posuere neque. Suspendisse interdum urna vel diam placerat, nec sodales velit posuere. Cras varius porttitor pellentesque. Curabitur auctor, lorem vel feugiat rhoncus, mi purus condimentum lectus, id iaculis augue nisl quis lectus. Donec ac metus in metus lobortis eleifend. Aliquam sagittis sapien in venenatis cursus. Phasellus diam purus, blandit sed euismod at, tempus tempor ante. Pellentesque eget est imperdiet, vestibulum turpis eu, varius velit. Suspendisse et sem in ligula mattis tempus et id purus.</p>
+
+				<p>Nulla nec odio quis quam volutpat laoreet et sit amet massa. Mauris cursus viverra lacinia. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sapien elit, tempor et vulputate sit amet, sollicitudin in dolor. Fusce tortor erat, blandit sit amet ante a, semper blandit ante. Suspendisse dui nulla, rhoncus sed sapien ut, sagittis lobortis nisl. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In malesuada ornare risus vitae laoreet. Nam malesuada feugiat odio vitae vehicula. Integer ac diam non tortor auctor lobortis bibendum consectetur enim. Quisque tristique, enim sit amet pretium aliquet, orci arcu ornare nunc, consectetur maximus turpis nunc eget arcu.</p>
+
+				<p>Sed elit magna, vulputate ut iaculis vel, elementum vel nisi. Fusce nec ante vel diam posuere auctor. Aliquam sodales iaculis nunc, vel lacinia massa. Nunc id sapien a erat ornare aliquet. Fusce quis semper est. In sed ligula ut diam fringilla tempor ut sit amet ipsum. Pellentesque at ex ut lacus euismod placerat quis a libero. In sem tortor, rutrum eu vulputate vitae, bibendum at libero.</p>
+
+				<p>Proin rutrum, orci at semper ornare, metus sem vulputate sapien, non fermentum metus nulla eu nisi. Curabitur ultricies eros nec euismod egestas. Aliquam ultricies, augue vel condimentum varius, turpis ante consequat tortor, sit amet porta purus nunc et nunc. Phasellus viverra vitae ante eget tristique. Ut ornare augue sed lectus fermentum, quis iaculis quam cursus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nunc dolor lectus, gravida consequat hendrerit quis, ultricies sit amet quam. Etiam venenatis rutrum erat, vitae venenatis nunc sagittis et. Cras pharetra scelerisque magna, ac tempor lacus rutrum ac. Quisque vitae diam eleifend metus fermentum eleifend. Mauris eu tortor leo. Donec vel elit enim. Proin rhoncus iaculis velit nec consectetur.</p>
+			</div>			
+		);
+
+	}
+
+}
