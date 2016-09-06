ec03ff128f0a5f422c27ff0f41fadeeada746790
Raphael Kottakal <raphaelsemail@gmail.com>
2016-09-06 17:04:24 +0530

    create window scroll event and callback

--- src/components/OnVisible.js
+++ src/components/OnVisible.js
@@ -1,13 +1,46 @@
 import React from 'react';
 
 export default class OnVisible extends React.Component {
+
+	constructor() {
+		super();
+		this.state = {
+			revealed: false
+		}
+	}
+
 	componentDidMount() {
-		console.log('mounted');
+		console.log('Mounted');
+		window.addEventListener('scroll', this.handelScroll.bind(this));
+	}
+
+	componentWillUnmount() {
+		console.log('Unmounted');
+		window.removeEventListener('scroll', this.handelScroll.bind(this));
+	}
+
+	handelScroll() {
+		if (!this.state.revealed) {
+			const windowOffset = window.pageYOffset + window.innerHeight;
+			const top = this.refs.topElem.offsetTop;
+			const height = this.refs.topElem.offsetHeight;
+			const endOffset = top + height;
+			// console.log('window offset', windowOffset);
+			// console.log('window height', window.innerHeight);
+			// console.log('end offset', this.endOffset);
+			// console.log('top', this.top);
+			// console.log('height', this.height);
+			if (windowOffset >= endOffset ) {
+				this.setState({ revealed: true });
+				console.log('offset reached');
+			}
+			console.log('Scrolled');
+		}
 	}
 
 	render() {
 		return(
-			<div>
+			<div ref="topElem">
 				{this.props.children}
 			</div>			
 		);
